b0eb832642b9e255938d20c19950bc6739f735fa
MeitalGuy <43933302+MeitalGuy@users.noreply.github.com>
2018-10-20 21:49:17 +0300

    Update moed_2018_aviv_b.java

--- moed_2018_aviv_b.java
+++ moed_2018_aviv_b.java
@@ -30,3 +30,51 @@ public static char findFirstUnique(String str)
 		counts[str.charAt(i)]++;
 	}
 }
+
+//q4
+    public static boolean P(int arr[], int n) 
+    {
+        return P(arr,n, 0, 1);
+    }
+
+    private static boolean P(int arr[], int n, int i, int num)
+    {
+        if(i+num+1>=arr.length)
+            return false;
+        if(num > n)
+            return true;
+        if(arr[i] == 0 && arr[i+num+1] == 0)
+        {
+            arr[i] = num;
+            arr[i+num+1]=num;
+            boolean opt1 = P(arr,n,0,num+1);
+            if(opt1)
+                return true;
+            else
+            {
+                arr[i] = 0;
+                arr[i+num+1] = 0;
+            }
+        }
+        boolean opt2 = P(arr,n,i+1,num);
+        return opt2;
+    }
+
+//Test for q4
+    public static void main(String args[])
+    {
+        for (int i=1; i<11; i++) 
+        {
+            int arr[] = new int[2*i];
+            boolean res = P(arr, i);
+            System.out.print("n="+i+": " + res);
+            if (res)
+            {
+                System.out.print(" [");
+                for (int j=0; j < arr.length - 1; j++)
+                    System.out.print(arr[j] + ", ");
+                System.out.print(arr[arr.length-1] + "]");
+            }
+            System.out.println();
+        }
+    }
