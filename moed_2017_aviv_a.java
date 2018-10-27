17a952455358af2cff13c3b492afe05484edeaac
MeitalGuy <43933302+MeitalGuy@users.noreply.github.com>
2018-10-27 21:33:33 +0300

    Add files via upload

---
+++ moed_2017_aviv_a.java
@@ -0,0 +1,28 @@
+//q3
+public static int findSingle(String[] arr){
+	int low = 0;
+	int high = arr.length-1;
+	int mid;
+	while(low<=high)
+	{
+		mid = low + high / 2;
+		if(arr[mid].equals(arr[mid-1]))
+		{
+			if(mid%2==0)
+				high = mid- 1;
+			else
+				low = mid + 1;
+		}
+		else if(arr[mid].equals(arr[mid+1]))
+		{
+			if(mid%2==0)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		else
+			return mid;
+	}
+	return -1;
+}
+//Time complexity O(m*log(n))
