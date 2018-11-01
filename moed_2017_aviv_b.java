f07d74fcd27547d0c916b16c1d659a3c581192fb
MeitalGuy <43933302+MeitalGuy@users.noreply.github.com>
2018-11-01 15:56:16 +0200

    Update moed_2017_aviv_b.java

--- moed_2017_aviv_b.java
+++ moed_2017_aviv_b.java
@@ -16,3 +16,29 @@ public static int identifyIndex(int arr[])
 	}
 	return -1;
 }
+
+//q3
+public static int countBalance(String str)
+{
+	final int SIZE = 26;
+	int leftArr[] = new int [SIZE];
+	int rightArr[] = new int [SIZE];
+	int count = 0;
+	for(int i = 0; i<str.length(); i++)
+	{
+		rightArr[str.charAt(i)-'a'] ++;
+	}
+	for(int i = 0; i<str.length(); i++)
+	{
+		leftArr[str.charAt(i) - 'a']++;
+		rightArr[str.charAt(i)-'a'] --;
+		boolean isBalance = true;
+		for(int j = 0; j<leftArr.length; j++)
+			if(leftArr[j] == 0 && rightArr[j] >0 || leftArr[j] >0 && rightArr[j] == 0)
+				isBalance = false;
+		if(isBalance)
+			count++;
+	}
+	return count;
+}
+//Time complexity O(n)
