4c3c6253545ce6856f91c811a3f4075e5e3f5172
MeitalGuy <43933302+MeitalGuy@users.noreply.github.com>
2018-11-03 20:31:03 +0200

    Update moed_2017_winter_b.java

--- moed_2017_winter_b.java
+++ moed_2017_winter_b.java
@@ -12,3 +12,25 @@ public static int findDominant(int a[], int x)
 	return x;
 }
 ///time complexity = O(n)
+
+//q3
+public static int findMin(String arr[])
+{
+	int low = 0;
+	int high = arr.length-1;
+	int mid;
+	if(arr[0].compareTo(arr[high]) < 0)
+		return 0;
+	while(low<=high)
+	{
+		mid = (low + high)/2;
+		if(arr[mid-1].compareTo(arr[mid]) > 0)
+			return mid;
+		if(arr[0].compareTo(arr[mid]) > 0)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return -1;
+}
+//time complexity O(m log n)
