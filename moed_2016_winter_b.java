9e14a31485fd1cc5f5f1f0f53ff61caf4bcf9f0f
MeitalGuy <43933302+MeitalGuy@users.noreply.github.com>
2018-11-09 21:52:33 +0200

    Update moed_2016_winter_b.java

--- moed_2016_winter_b.java
+++ moed_2016_winter_b.java
@@ -18,5 +18,34 @@ public static String searchAndReplace(String str, String search, String replace)
 			res = res + str.charAt(i);
 	}
 	return res;
-
+}
 //Time complexity O(n)
+	
+//q3
+public static int factors(int num)
+{
+	int count = 0;
+	for(int i = 0; i*i<num; i++)
+	{
+		if(num%i == 0)
+			count = count + 2;
+	}
+	if(i*i == num)
+		count++;
+	return count;
+	
+}
+//Time complexity O(sqrt(n))
+//q3
+public static boolean findKFctors(int arr[], int k)
+{
+	return findKFctors(arr,k,0,0);
+}
+private static boolean findKFctors(int arr[], int k, int i,int sum)
+{
+	if(i==arr.length)
+		return factors(sum) == k;
+	boolean opt1 = findKFctors(arr,k,i+1,sum+arr[i]);
+	boolean opt2 = findKFctors(arr,k,i+1,sum);
+	return opt1||opt2;
+}	
